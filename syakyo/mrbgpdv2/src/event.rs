de332dc89dc434034c927f3c6b71d9bbe1b4854c
Daichi Mukai <daichi@mukai.dev>
2022-12-17 19:24:21 +0900

    mrbgpdv2: transition to Connect state
    Ref: chapter 2.5

---
+++ syakyo/mrbgpdv2/src/event.rs
@@ -0,0 +1,5 @@
+// https://www.rfc-editor.org/rfc/rfc4271.html#section-8.1.2
+#[derive(PartialEq, Eq, Debug, Clone, Hash)]
+pub enum Event {
+    ManualStart,
+}
---
+++ syakyo/mrbgpdv2/src/lib.rs
@@ -0,0 +1,10 @@
+#![feature(exclusive_range_pattern, arc_unwrap_or_clone)]
+#![allow(dead_code)]
+
+mod bgp_type;
+pub mod config;
+mod error;
+mod event;
+mod event_queue;
+pub mod peer;
+mod state;
