5909355570c5668e31a233f46120ec7e1b02ed77
Daichi Mukai <daichi@mukai.dev>
2023-03-12 22:54:50 +0900

    teio: refactor: add job.go

--- sandbox/teio/cmd/teio/main.go
+++ sandbox/teio/cmd/teio/main.go
@@ -2,88 +2,12 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	"io"
 	"log"
 	"os"
-	"sort"
 	"sync"
-	"time"
+
+	"github.com/daichimukai/x/sandbox/teio"
 )
-
-const (
-	targetFilenameFormat = "teio.%d"
-)
-
-type Job struct {
-	id        int
-	blockSize int
-	fileSize  int
-	fp        *os.File
-}
-
-func NewJob(id int, blockSize int, fileSize int) (*Job, error) {
-	filename := fmt.Sprintf(targetFilenameFormat, id)
-	fp, err := os.OpenFile(filename, os.O_CREATE|os.O_RDWR|os.O_TRUNC|os.O_SYNC, 0644)
-	if err != nil {
-		return nil, fmt.Errorf("failed to open file: %w", err)
-	}
-
-	return &Job{
-		id:        id,
-		blockSize: blockSize,
-		fileSize:  fileSize,
-		fp:        fp,
-	}, nil
-}
-
-func (j *Job) Do() (*JobResult, error) {
-	count := j.fileSize / j.blockSize
-	lats := make([]time.Duration, count)
-
-	b := make([]byte, j.blockSize)
-	for i := 0; i < count; i++ {
-		start := time.Now()
-		_, err := j.fp.Write(b[:])
-		if err != nil {
-			return nil, err
-		}
-		end := time.Now()
-		lats[i] = end.Sub(start)
-	}
-	sort.Slice(lats, func(i, j int) bool { return lats[i] < lats[j] })
-
-	j.fp.Close()
-
-	return &JobResult{job: j, lats: lats}, nil
-}
-
-type JobResult struct {
-	job *Job
-
-	lats []time.Duration
-}
-
-func (j JobResult) PrettyPrint(w io.Writer) {
-	var latSum time.Duration
-	for _, lat := range j.lats {
-		latSum += lat
-	}
-	count := len(j.lats)
-	writtenMiB := float64(count*j.job.blockSize) / 1024 / 1024
-	throughputMiBs := writtenMiB / float64(latSum.Microseconds()) * 1000 * 1000
-	iops := float64(count) / float64(latSum.Microseconds()) * 1000 * 1000
-
-	fmt.Fprintf(w, "job%d: \n", j.job.id)
-	fmt.Fprintf(w, "  block size: %d byte\n", j.job.blockSize)
-	fmt.Fprintf(w, "  total bytes written: %.02f MiB\n", writtenMiB)
-	fmt.Fprintf(w, "  throughput: %.02f MiB/s, %.02f IOPS\n", throughputMiBs, iops)
-	fmt.Fprintf(w, "  latency:\n")
-	fmt.Fprintf(w, "    avg: %d usec\n", latSum.Microseconds()/int64(count))
-	fmt.Fprintf(w, "    50%%: %d usec\n", j.lats[count/2].Microseconds())
-	fmt.Fprintf(w, "    90%%: %d usec\n", j.lats[count*90/100].Microseconds())
-	fmt.Fprintf(w, "    99%%: %d usec\n", j.lats[count*99/100].Microseconds())
-}
 
 var (
 	numJobs   = flag.Int("num-jobs", 1, "the number of jobs which run concurrently")
@@ -94,11 +18,11 @@ var (
 func main() {
 	flag.Parse()
 
-	jobs := make([]*Job, *numJobs)
-	results := make([]*JobResult, *numJobs)
+	jobs := make([]*teio.Job, *numJobs)
+	results := make([]*teio.JobResult, *numJobs)
 
 	for i := 0; i < *numJobs; i++ {
-		job, err := NewJob(i, *blockSize, *fileSize)
+		job, err := teio.NewJob(i, *blockSize, *fileSize)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -109,7 +33,7 @@ func main() {
 	var wg sync.WaitGroup
 	for i, job := range jobs {
 		wg.Add(1)
-		go func(i int, job *Job) {
+		go func(i int, job *teio.Job) {
 			<-startCh
 
 			result, err := job.Do()
